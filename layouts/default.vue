cfde69ee81b30abf845f61f26e8c1ce39799b0ad
AdipatS <66309010019@tatc.ac.th>
2023-08-21 08:49:05 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">about me</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/limitmessage">Limit Message</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/previewImage">Preview Image</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+    <slot />
+    <footer class="mt-2">
+        <nav data-bs-theme="dark">
+        <div class="card text-center">
+        <div class="card-header">
+        Footer
+        </div>
+        <div class="card-body">
+        <h5>TATC</h5>
+        <h5 class="card-title">Adipat Sompong</h5>
+        <p class="card-text">66309010019 IT.4/1</p>
+        </div>
+        </div>
+        </nav>  
+    </footer>
+</template>
+<style>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="text-center mt-2">
+        <img :src="picture" :width="size" :height="auto"> 
+        <p class="mt-2">ชื่อ :{{ getFullname() }}</p>
+        <p>นามสกุล :{{ lastname }}</p>
+        <p>อายุ : {{ age }}</p>
+        <p>ที่อยู่ : <span v-html="address"></span></p>
+        <p v-if="hobby.length ===0">ไม่มีงานอดิเรก</p>
+        <div v-else>
+        <p>งานอดิเรก :</p>
+            <ul>
+                <li v-for="item in hobby">{{ item }}</li>
+            </ul>
+        </div>
+        <p>ข้อมูลพื้นฐาน : </p>
+            <ul>
+                <li v-for="(value,key) in general" :key="key">{{ key }}:{{ value }}</li>
+            </ul>
+                <button @click="showData">คลิกเพื่อดูข้อมูล</button>
+                <button @click="increment(10)">เพิ่มค่า</button>
+                <button @click="decrement(3)">ลดค่า</button>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                firstname: "Adipat",
+                lastname: "Sompong",
+                age: 18,
+                address: "<i>Chonburi</i>",
+                picture:"http://www.106planet.com/66309010019/img/FB_IMG_1688624333586.jpg",
+                size:250,
+                hobby : ["ทำไร่","ทำสวน","นอน","นอน","นอน"],
+                general : {เพศ:"ชาย",น้ำหนัก:95,ส่วนสูง:174,โรคประจำตัว:false}
+            }
+        },
+        methods:{
+            getFullname(){
+                return `${this.firstname} ${this.lastname}`
+            },
+            showData(){
+                alert(this.firstname)
+            },
+            increment(value){
+                this.age += value
+            },
+            decrement(value){
+                this.age -= value
+            },
+            setNickName(event){
+                this.nickname=event.target.value
+            },
+            submitForm(e){
+                e.preventDefault();
+                alert('บันทึกข้อมูลเรียบร้อย')
+            }
+        }
+    }
+    
+</script>
